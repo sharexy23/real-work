3344b5de2a9af3bac1fc1c53638c0d0e8e991520
sharexy23 <omotietie@yahoo.com>
2021-03-04 23:37:36 +0100

    DFG

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import JWTManager
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '!@#$%^&*()_+=-0987654321'
 app.config['PROPAGATE_EXCEPTIONS'] = True
